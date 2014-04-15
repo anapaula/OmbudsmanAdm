92783587b69824a05e7d118ffa61c6b3771dd0da
anapaula <anapaula_spbr@yahoo.com.br>
2014-04-15 07:23:51 -0300

    Inclusão de interface IncidentDAO e da classe que a implementa com SQLite com os métodos insert, update, delete e getIncidentById.

---
+++ src/br/com/usp/ime/ombudsmanadm/model/dao/IncidentDAO.java
@@ -0,0 +1,19 @@
+package br.com.usp.ime.ombudsmanadm.model.dao;
+
+import java.util.List;
+
+import android.database.sqlite.SQLiteDatabase;
+import br.com.usp.ime.ombudsmanadm.model.vo.Incident;
+
+public interface IncidentDAO {
+	
+	public void onCreate(SQLiteDatabase database);
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion);
+	public Incident getIncidentById(Long id);
+	public List<Incident> getIncidents();
+	public List<Incident> getIncidentsByKeyValue(String key);
+	public void insert(Incident incident);
+	public void update(Incident incident);
+	public void delete(Incident incident);
+	public void close();
+}
---
+++ src/br/com/usp/ime/ombudsmanadm/model/dao/IncidentSqLiteDAO.java
@@ -0,0 +1,124 @@
+package br.com.usp.ime.ombudsmanadm.model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import br.com.usp.ime.ombudsmanadm.model.vo.Incident;
+
+public class IncidentSqLiteDAO extends SQLiteOpenHelper implements IncidentDAO {
+
+	private static final String DATABASE = "OMBUDSMAN_DB";
+	private static final String TABLE_NAME = "INCIDENT";
+	private static final int VERSION = 1;
+
+	private static final String[] COLS = { "id", "uspNumber", "description",
+			"localization", "latitude", "longitude", "photo", "createdAt",
+			"updatedAt" };
+
+	public IncidentSqLiteDAO(Context context) {
+		super(context, DATABASE, null, VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String ddl = "CREATE TABLE "
+				+ TABLE_NAME
+				+ "(id INTEGER PRIMARY KEY"
+				+ ", uspNumber INTEGER NOT NULL, description TEXT NOT NULL, localization TEXT NOT NULL"
+				+ ", latitude REAL NOT NULL, longitude REAL NOT NULL, photo BLOB NOT NULL,"
+				+ " createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL);";
+
+		db.execSQL(ddl);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+		db.execSQL(sql);
+		onCreate(db);
+	}
+
+	public List<Incident> getIncidents() {
+		List<Incident> incidents = new ArrayList<Incident>();
+
+		Cursor cs = getWritableDatabase().query(TABLE_NAME, COLS, null, null,
+				null, null, null);
+
+		while (cs.moveToNext()) {
+			Incident incident = getIncidentFromCursor(cs);
+			incidents.add(incident);
+		}
+		cs.close();
+
+		return incidents;
+	}
+
+	public List<Incident> getIncidentsByKeyValue(String key) {
+		List<Incident> incidents = new ArrayList<Incident>();
+
+		Cursor cs = getReadableDatabase()
+				.rawQuery(
+						"SELECT * FROM " + TABLE_NAME
+								+ " WHERE description like '%?%'",
+						new String[] { key });
+
+		while (cs.moveToNext()) {
+			Incident incident = getIncidentFromCursor(cs);
+			incidents.add(incident);
+		}
+		cs.close();
+
+		return incidents;
+	}
+
+	private Incident getIncidentFromCursor(Cursor cs) {
+		Incident incident = new Incident();
+		incident.setId(cs.getLong(0));
+		incident.setUspNumber(cs.getLong(1));
+		incident.setDescription(cs.getString(2));
+		incident.setLocalization(cs.getString(3));
+		incident.setLatitude(cs.getFloat(4));
+		incident.setLongitude(cs.getFloat(5));
+		incident.setPhoto(cs.getBlob(6));
+		incident.setCreatedAt(cs.getString(7));
+		incident.setUpdatedAt(cs.getString(8));
+
+		return incident;
+	}
+
+	@Override
+	public Incident getIncidentById(Long id) {
+		Cursor c = getWritableDatabase().query(TABLE_NAME, COLS, "id=?",
+				new String[] { id.toString() }, null, null, null);
+		c.moveToFirst();
+		Incident incident = getIncidentFromCursor(c);
+		c.close();
+		return incident;
+	}
+
+	@Override
+	public void insert(Incident incident) {
+		ContentValues values = incident.toContentValues();
+		getWritableDatabase().insert(TABLE_NAME, null, values);
+	}
+
+	@Override
+	public void update(Incident incident) {
+		ContentValues values = incident.toContentValues();
+		getWritableDatabase().update(TABLE_NAME, values, "id=?",
+				new String[] { incident.getId().toString() });
+
+	}
+
+	@Override
+	public void delete(Incident incident) {
+		getWritableDatabase().delete(TABLE_NAME, "id=?",
+				new String[] { incident.getId().toString() });
+	}
+
+}
