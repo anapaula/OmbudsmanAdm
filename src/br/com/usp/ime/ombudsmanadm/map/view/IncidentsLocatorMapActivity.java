a2f9a5e6e4dcead35d7a4f488b54d0c7742c7bec
anapaula <anapaula_spbr@yahoo.com.br>
2014-04-16 13:48:41 -0300

    Inclusão de Activity para exibição de mapa com marcadores de incidentes registrados.

---
+++ src/br/com/usp/ime/ombudsmanadm/map/view/IncidentsLocatorMapActivity.java
@@ -0,0 +1,68 @@
+package br.com.usp.ime.ombudsmanadm.map.view;
+
+import br.com.usp.ime.ombudsmanadm.R;
+import br.com.usp.ime.ombudsmanadm.R.id;
+import br.com.usp.ime.ombudsmanadm.R.layout;
+import br.com.usp.ime.ombudsmanadm.R.menu;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class IncidentsLocatorMapActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_incidents_locator_map);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.incidents_locator_map, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(
+					R.layout.fragment_incidents_locator_map, container, false);
+			return rootView;
+		}
+	}
+
+}
